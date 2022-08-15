5c5dfb05c49dc8a6270efb84689a7b33d91a7f15
Manuel Rüger <manuel@rueg.eu>
2022-08-15 21:03:07 +0200

    Switch to kingpin for flag parsing

--- go.mod
+++ go.mod
@@ -8,11 +8,13 @@ require (
 	github.com/prometheus/client_golang v1.13.0
 	github.com/prometheus/common v0.37.0
 	github.com/prometheus/exporter-toolkit v0.7.1
-	github.com/spf13/pflag v1.0.5
 	github.com/xhit/go-str2duration/v2 v2.0.0
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6
 )
 
 require (
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
+	github.com/alecthomas/units v0.0.0-20190924025748-f65c72e2690d // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/go-logfmt/logfmt v0.5.1 // indirect
--- main.go
+++ main.go
@@ -16,14 +16,14 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/prometheus/common/version"
 	"github.com/prometheus/exporter-toolkit/web"
-	"github.com/spf13/pflag"
+	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 var (
-	loginName = pflag.String("login-name", "", "User ID")
-	password  = pflag.String("password", "", "API Password")
-	addr      = pflag.String("listen-address", ":9757", "The address to listen on for HTTP requests.")
-	tlsConfig = pflag.String("tls-config", "", "Path to TLS config file.")
+	loginName = kingpin.Flag("login-name", "User ID").Envar("SCP_LOGINNAME").Default("").String()
+	password  = kingpin.Flag("password", "API Password").Envar("SCP_PASSWORD").Default("").String()
+	addr      = kingpin.Flag("listen-address", "The address to listen on for HTTP requests.").Envar("SCP_LISTENADDRESS").Default(":9757").String()
+	tlsConfig = kingpin.Flag("tls-config", "Path to TLS config file.").Envar("SCP_TLSCONFIG").Default("").String()
 )
 
 const netcupWSUrl = "https://www.servercontrolpanel.de/SCP/WSEndUser"
@@ -32,19 +32,20 @@ func main() {
 	var logger log.Logger
 	logger = log.NewLogfmtLogger(log.NewSyncWriter(os.Stderr))
 	logger = log.With(logger, "ts", log.DefaultTimestampUTC, "caller", log.DefaultCaller)
+	kingpin.Version(version.Version + " git " + version.Revision)
+	kingpin.Parse()
 	_ = logger.Log("msg", "Starting SCP Exporter version "+version.Version+" git "+version.Revision)
-	pflag.Parse()
 	client := soap.NewClient(netcupWSUrl)
 	wsclient := scpclient.NewWSEndUser(client)
 	scpCollector := metrics.NewScpCollector(wsclient, logger, loginName, password)
 	prometheus.DefaultRegisterer.MustRegister(scpCollector)
 	prometheus.DefaultRegisterer.MustRegister(version.NewCollector("scp"))
-        metricsServer := http.Server{Handler: promhttp.HandlerFor(
-                prometheus.DefaultGatherer,
-                promhttp.HandlerOpts{
-                        // Opt into OpenMetrics to support exemplars.
-                        EnableOpenMetrics: true,
-                }), Addr: *addr}
+	metricsServer := http.Server{Handler: promhttp.HandlerFor(
+		prometheus.DefaultGatherer,
+		promhttp.HandlerOpts{
+			// Opt into OpenMetrics to support exemplars.
+			EnableOpenMetrics: true,
+		}), Addr: *addr}
 	http.Handle("/", http.RedirectHandler("/metrics", http.StatusFound))
 	err := web.ListenAndServe(&metricsServer, *tlsConfig, logger)
 	if err != nil {
