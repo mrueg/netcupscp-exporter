79b0e0b2fa7ea2de24f81250cc93a51bcf7fafa4
Manuel Rüger <manuel@rueg.eu>
2023-02-27 17:39:48 +0100

    Bump dependencies

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/prometheus/common/version"
 	"github.com/prometheus/exporter-toolkit/web"
-	"gopkg.in/alecthomas/kingpin.v2"
+	"github.com/alecthomas/kingpin/v2"
 )
 
 var (
