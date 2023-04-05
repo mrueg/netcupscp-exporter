dc118b827d720145f2c06c945ae915fc46728252
Manuel Rüger <manuel@rueg.eu>
2023-04-05 14:13:06 +0200

    Implement landing page

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"os"
 	"time"
 
+	"github.com/alecthomas/kingpin/v2"
 	"github.com/go-kit/log"
 	"github.com/go-kit/log/level"
 	"github.com/hooklift/gowsdl/soap"
@@ -19,7 +20,6 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/prometheus/common/version"
 	"github.com/prometheus/exporter-toolkit/web"
-	"github.com/alecthomas/kingpin/v2"
 )
 
 var (
@@ -38,6 +38,8 @@ func main() {
 	kingpin.Parse()
 
 	var logger log.Logger
+
+	var metricsPath = "/metrics"
 	logger = log.NewLogfmtLogger(log.NewSyncWriter(os.Stderr))
 	logger = level.NewFilter(logger, level.Allow(level.ParseDefault(*logLevel, level.InfoValue())))
 	logger = log.With(logger, "ts", log.DefaultTimestampUTC, "caller", log.DefaultCaller)
@@ -48,21 +50,37 @@ func main() {
 	prometheus.DefaultRegisterer.MustRegister(scpCollector)
 	prometheus.DefaultRegisterer.MustRegister(version.NewCollector("scp"))
 	metricsServer := http.Server{
-		Handler: promhttp.HandlerFor(
-			prometheus.DefaultGatherer,
-			promhttp.HandlerOpts{
-				// Opt into OpenMetrics to support exemplars.
-				EnableOpenMetrics: true,
-			}),
-
 		ReadHeaderTimeout: 5 * time.Second}
-	http.Handle("/", http.RedirectHandler("/metrics", http.StatusFound))
+
+	landingConfig := web.LandingConfig{
+		Name:        "Netcup SCP Exporter",
+		Description: "Exporting Metrics from Netcup's ServerControlPanel",
+		Version:     version.Version + " git " + version.Revision,
+		Links: []web.LandingLinks{
+			{
+				Address: metricsPath,
+				Text:    "Metrics",
+			},
+		},
+	}
+	landingPage, err := web.NewLandingPage(landingConfig)
+	if err != nil {
+		_ = level.Error(logger).Log(err, "failed to create landing page")
+	}
+	http.Handle(metricsPath, promhttp.HandlerFor(
+		prometheus.DefaultGatherer,
+		promhttp.HandlerOpts{
+			// Opt into OpenMetrics to support exemplars.
+			EnableOpenMetrics: true,
+		}))
+	http.Handle("/", landingPage)
+
 	flags := web.FlagConfig{
 		WebListenAddresses: &[]string{*addr},
-		WebSystemdSocket: new(bool),
-		WebConfigFile: tlsConfig,
+		WebSystemdSocket:   new(bool),
+		WebConfigFile:      tlsConfig,
 	}
-	err := web.ListenAndServe(&metricsServer, &flags, logger)
+	err = web.ListenAndServe(&metricsServer, &flags, logger)
 	if err != nil {
 		_ = level.Error(logger).Log("msg", "Run into bad state", "error", err)
 		os.Exit(1)
